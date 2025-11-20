f0219cd8dc3b909aa31a70117079ae76abdef777
hqluke <212039062+hqluke@users.noreply.github.com>
2025-11-19 18:43:18 -0700

    Have access to images through API

--- src/Memory.jsx
+++ src/Memory.jsx
@@ -6,24 +6,50 @@ import { useEffect } from "react";
 
 function Memory() {
     const [count, setCount] = useState(0);
-    const [pokemonArray, setPokemonArray] = useState([]);
+    const [pokemonData, setPokemonData] = useState([]);
+    const [pokemonFinal, setPokemonFinal] = useState([]);
 
-    const fetchPokemon = () => {
-        fetch("https://pokeapi.co/api/v2/pokemon?limit=9")
-            .then((response) => response.json())
-            .then((data) => {
-                setPokemonArray(data.results);
-            });
+    // Fetch initial Pokemon list
+    const fetchPokemon = async () => {
+        const response = await fetch(
+            "https://pokeapi.co/api/v2/pokemon?limit=12",
+        );
+        const data = await response.json();
+        setPokemonData(data.results);
     };
 
+    // Fetch details for each Pokemon
+    const fetchPokemonDetails = async (pokemonList) => {
+        const promises = pokemonList.map(async (poke) => {
+            const response = await fetch(poke.url);
+            const data = await response.json();
+            return {
+                name: data.name,
+                image: data.sprites.front_default,
+                url: poke.url,
+            };
+        });
+
+        const results = await Promise.all(promises);
+        setPokemonFinal(results);
+    };
+
+    // Initial fetch
     useEffect(() => {
         fetchPokemon();
     }, []);
 
+    // Fetch details when pokemonData changes
     useEffect(() => {
-        console.log("Updated pokemon array:", pokemonArray);
-    }, [pokemonArray]);
+        if (pokemonData.length > 0) {
+            console.log("Updated pokemon array:", pokemonData);
+            fetchPokemonDetails(pokemonData);
+        }
+    }, [pokemonData]);
 
+    useEffect(() => {
+        console.log("Updated pokemonFinal:", pokemonFinal);
+    }, [pokemonFinal]);
     return (
         <>
             <div>
